ba201dbaf251db113df63d44d30d433552dc2425
Jacob <Yoimithrandir@gmail.com>
2026-07-21 20:05:35 +0800

    create man.py

--- board.py
+++ board.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 class MinesweeperBoard:
-    def __init__(self,rows:int,columns:int,num_of_mines:int)->None:
+    def __init__(self,rows:int,columns:int,num_of_mines:int,seed=50)->None:
         self.rows=rows
         self.columns=columns
         self.num_of_mines=num_of_mines
         self.board = np.zeros((self.rows, self.columns), dtype=int)
+        self.rng=np.random.default_rng(seed=seed)
         self.generate()
 
     #validation check
@@ -13,7 +14,7 @@ class MinesweeperBoard:
     def rows(self):
         return self._rows
     @rows.setter
-    def rows(self,rows):
+    def rows(self,rows:int):
         if not isinstance(rows,int):
             raise TypeError("rows should be integer")
         if rows<=0:
@@ -25,7 +26,7 @@ class MinesweeperBoard:
     def columns(self):
         return self._columns
     @columns.setter
-    def columns(self,columns):
+    def columns(self,columns:int):
         if not isinstance(columns,int):
             raise TypeError("columns should be integer")
         if columns<=0:
@@ -37,7 +38,7 @@ class MinesweeperBoard:
     def num_of_mines(self):
         return self._num_of_mines
     @num_of_mines.setter
-    def num_of_mines(self,num_of_mines):
+    def num_of_mines(self,num_of_mines:int):
         if not isinstance(num_of_mines,int):
             raise TypeError("nums of mines should be integer")
         if num_of_mines<=0:
@@ -46,34 +47,40 @@ class MinesweeperBoard:
             raise ValueError("Too many mines for the board")
         self._num_of_mines=num_of_mines
 
+    #以下getter方便调用数据
     @property
     def shape(self):
         return self.board.shape
+    @property
+    def data(self):
+        return self.board
+    
     #随机选一定区域指定为雷
     def place_mines(self)->None:
-        mines_array=np.random.choice(self.rows*self.columns,size=self.num_of_mines,replace=False)
+        mines_array=self.rng.choice(self.rows*self.columns,size=self.num_of_mines,replace=False)
         self.board.reshape(-1)[mines_array] = -1
     #计算雷附近的数字
-    def calculate_numbers(self):
-        direction=[(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+    def calculate_numbers(self)->None:
         mines_r,mines_c=np.where(self.board<0)
+
         is_mine=(self.board==-1)
-        for dr,dc in direction:
-            new_r,new_c=mines_r+dr,mines_c+dc
-            valid=(new_r>=0) & (new_r<=self.rows-1) & (new_c>=0) & (new_c<=self.columns-1)
-            np.add.at(self.board,(new_r[valid],new_c[valid]),1)
-        
+        pad_mines=np.pad(self.board,1,'constant',constant_values=0)
+        for r,c in zip(mines_r,mines_c):
+            r,c=r+1,c+1       #pad之后加上offset
+            pad_mines[r-1:r+2,c-1:c+2]+=1           #雷周围一圈数字+1
+        self.board=pad_mines[1:self.rows+1,1:self.columns+1]
         self.board[is_mine]=-1
-    
-    def generate(self):
+    #生成地图
+    def generate(self)->None:
         self.place_mines()
         self.calculate_numbers()
-
-    def __str__(self):
+    #打印地图
+    def __str__(self)->str:
         str_board=self.board.astype(str)+' '
         str_board[str_board=='-1 ']='* '
         return '\n'.join(''.join(row) for row in str_board)
         
-My_Board=MinesweeperBoard(20,20,20)
-print(My_Board)
+if __name__=="__main__":
+    My_Board=MinesweeperBoard(20,20,20,seed=80)
+    print(My_Board)
             
--- game.py
+++ game.py
@@ -1,118 +1,132 @@
 import numpy as np
 from board import MinesweeperBoard
+#定义三种格子状态，hidden未翻开，open已翻开，flag插旗
+HIDDEN = 0      
+OPEN = 1
+FLAG = 2
+
 class MinesweeperGame:
-    def __init__(self,board):
-        self.cell_state={'hidden':0,'open':1,'flag':2}      #0代表隐藏，1代表显示（格子翻开），2代表插旗
-        self._visible_state=np.zeros(board.shape,dtype=int)   
+    def __init__(self,board:MinesweeperBoard)->None:
+
+        self._state=np.zeros(board.shape,dtype=int)   
         self.board=board
         self.game_over=False
+        self.direction=[(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
 
+    """
+    主要两个函数，模拟左右键点击地图效果
+    """
     #右键控制插旗
     def mouse2(self, position:tuple)->None:          
-        if self._visible_state[position]==self.cell_state['hidden']:    #给隐藏格子插旗
-            self._visible_state[position]=self.cell_state['flag']
-        elif self._visible_state[position]==self.cell_state['open']:    #对显示格子操作，无效操作
+        if self._state[position]==HIDDEN:    #给隐藏格子插旗
+            self._state[position]=FLAG
+        elif self._state[position]==OPEN:    #对已翻开格子操作，无效操作
             pass
-        else:                                                           #取消已插旗格子
-            self._visible_state[position]=self.cell_state['hidden']
+        else:                                #取消已插旗格子
+            self._state[position]=HIDDEN
         return 
     
-    #左键翻格子
-    def mouse1(self,position:tuple):
-        if self._visible_state[position]==self.cell_state['flag']:     #翻插旗格子，无效
+    #左键点击，进行点隐藏翻格子或点数字翻周围一片
+    def mouse1(self,position:tuple)->None:
+        if self._state[position]==FLAG:     #翻插了旗的格子，无效
             return
-        elif self._visible_state[position]==self.cell_state['hidden']: #翻开格子，判断是否是雷
-            self._visible_state[position]=self.cell_state['open']
-
-            if self.board.board[position]==0:           self.expand(position)    #展开周围一片非雷空白区域
-                
-            self.is_mine(position)
-        
-                                                                        #已经探明周围雷情况下点数字
-        elif self.board.board[position]>0 and self.board.board[position]==self.flag_count(position):                                                              
-            self.expand_around(position)
+        elif self._state[position]==HIDDEN: #翻开格子
+            self.open_cell(position)
+                                            #已经探明周围雷情况下点数字
+        elif self.board.data[position]>0 and self.board.data[position]==self.flag_count(position):                                                              
+            self.num_expand(position)
 
 
-
-
-    def is_mine(self,position):
-        if self.board.board[position]==-1:
+    """
+    其余工具函数，辅助判断
+    """
+    #如果翻到雷，game_over
+    def is_mine(self,position:tuple)->bool:
+        if self.board.data[position]==-1:
             self.game_over=True
         return self.board.board[position]==-1
     
     #统计该位置周围旗数
     def flag_count(self,position):
         r,c=position
-        pad_state = np.pad(
-            self._visible_state,
-            pad_width=1,
-            constant_values=self.cell_state['hidden']
-        )
+        pad_state = np.pad(self._state,pad_width=1,constant_values=HIDDEN)
         r,c=r+1,c+1
         window=pad_state[r-1:r+2,c-1:c+2]
-        return (window==self.cell_state['flag']).sum()
+        return (window==FLAG).sum()
 
     #翻开周围8个格子
-    def expand_around(self,position)->None:
-        direction=[(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+    def num_expand(self,position:tuple)->None:
         r,c=position
-
-        for dr,dc in direction:
+        for dr,dc in self.direction:
             new_r,new_c=r+dr,c+dc
-            valid=(new_r>=0) and (new_r<=self.board.rows-1) and (new_c>=0) and (new_c<=self.board.columns-1)
-            if valid:
-                self.mouse1((new_r,new_c))
+            if not self.is_valid((new_r,new_c)): continue
 
-    #翻到0，会连续翻一片空白区域
-    def expand(self,position):
-        direction=[(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+            if self._state[new_r,new_c]==FLAG: continue    
+            elif self._state[position]==HIDDEN: self.open_cell(position)
+
+
+    #翻到空格，会连续翻一片空白区域
+    def blank_expand(self,position):
         r,c=position
-        for dr,dc in direction:
+        for dr,dc in self.direction:
             new_r,new_c=r+dr,c+dc
-            valid=(new_r>=0) and (new_r<=self.board.rows-1) and (new_c>=0) and (new_c<=self.board.columns-1)
-            if not valid:    continue
 
-            if self._visible_state[new_r,new_c]==self.cell_state['open']: continue
+            if not self.is_valid((new_r,new_c)):    continue
 
-            if self._visible_state[new_r,new_c]==self.cell_state['flag']: continue
+            if self._state[new_r,new_c]==OPEN: continue
 
-            if self.board.board[new_r,new_c]==-1: continue
+            if self._state[new_r,new_c]==FLAG: continue
 
-            self._visible_state[new_r,new_c]=self.cell_state['open']   #翻开数字格或空格
-            if self.board.board[new_r,new_c]==0:                       #翻开空格，递归
-                self.expand((new_r,new_c))
+            #剩余为HIDDEN情况
+            self.open_cell((new_r,new_c))
     
+    #翻开单个隐藏格子，可能触发self.blank_expand()
+    def open_cell(self,position:tuple)->None:
+        self._state[position]=OPEN
+        self.is_mine(position)
+        if self.board.data[position]==0:   self.blank_expand(position)#翻到空格,额外展开周围一片非雷空白区域
+    
+    #检查当前位置是否越界
+    def is_valid(self,position:tuple)->bool:
+        r,c=position
+        return (0<= r<self.board.rows and 0<=c<self.board.columns)
+
     #显示当前局面情况
-    def show_visible_state(self):
+    def show_state(self)->None:
         str_board= np.full(self.board.shape, '?', dtype=str)
-        open_mask = self._visible_state == self.cell_state['open']
-        str_board[open_mask]=self.board.board[open_mask].astype(str)
-        flag_mask = self._visible_state == self.cell_state['flag']
+
+        open_mask = (self._state == OPEN)
+        str_board[open_mask]=self.board.data[open_mask].astype(str)
+
+        flag_mask = (self._state == FLAG)
         str_board[flag_mask]='🚩'
+
+        open_mine_mask=open_mask*(self.board.data==-1)
+        str_board[open_mine_mask]='💣'
 
         print('\n'.join(' '.join(row) for row in str_board))
 
 
 
 if __name__=="__main__":
-    board = MinesweeperBoard(10,10,10)
+    board = MinesweeperBoard(10,10,10,50)
     print("真实棋盘:")
     print(board)
     game = MinesweeperGame(board)
 
     print("初始:")
-    game.show_visible_state()
+    game.show_state()
 
     game.mouse1((0,0))
     print("点击(0,0):")
-    game.show_visible_state()
+    game.show_state()
 
-    game.mouse2((1,1))
-    print("插旗(4,1):")
-    game.show_visible_state()
+    game.mouse2((5,2))
+    print("插旗(5,2):")
+    game.show_state()
 
     game.mouse1((2,2))
     print("点击(3,1):")
-    game.show_visible_state()
+    game.show_state()
 
     print("game over:",game.game_over)
